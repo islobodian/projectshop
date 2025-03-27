edc74fd72c93d58c3630dd5d8e180cc8f0ae8065
islobodian <149094673+islobodian@users.noreply.github.com>
2025-03-27 20:13:08 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,27 @@ function scaleBox() {
     let box = document.getElementById('scaleBox');
     box.style.transform = box.style.transform === 'scale(1.5)' ? 'scale(1)' : 'scale(1.5)';
 }
+
+function toggleFade2() {
+    let box = document.getElementById('seemore2');
+    if (box.style.display == "none") {
+        box.style.display = "flex";
+    }
+    else {
+        box.style.display = "none";
+    }
+    
+
+}
+function toggleFade3() {
+    let box = document.getElementById('seemore3');
+    if (box.style.display == "none") {
+        box.style.display = "flex";
+    }
+    else {
+        box.style.display = "none";
+    }
+    
+
+}
+
